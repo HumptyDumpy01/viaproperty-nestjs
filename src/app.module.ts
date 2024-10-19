154b24dad40f7392aa1aae6469b8e79cc587adb1
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-19 08:36:43 +0300

    Connected to Mongo successfully.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  // path to .env.config
+  path: `${__dirname}/../../config.env`,
+});
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: `mongodb`,
+      url: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@viaproperty.ez3sz.mongodb.net/viaproperty?retryWrites=true&w=majority&appName=viaproperty`,
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
