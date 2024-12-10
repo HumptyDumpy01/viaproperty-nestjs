664969133e19bbe91e0e70aeff07cf9a82471649
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-10 15:44:54 +0200

    Added conditions to check if user/landlord or property exists

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { OrderResolver } from './order.resolver';
 import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order.entity';
+import { PropertyModule } from '../property/property.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   providers: [OrderResolver, OrderService],
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+    forwardRef(() => PropertyModule),
+    AuthModule,
+  ],
 })
 export class OrderModule {}
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './order.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput } from './inputs/create-order.input';
 import { v4 as uuid } from 'uuid';
+import { PropertyService } from '../property/property.service';
+import { AuthService } from '../auth/auth.service';
+import { showErrorMessage } from '../../utils/functions/showErrorMessage';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order)
     private orderRepository: Repository<Order>,
+    private propertyService: PropertyService,
+    private authService: AuthService,
   ) {}
 
   async getAllOrders(): Promise<Order[]> {
@@ -17,6 +22,27 @@ export class OrderService {
   }
 
   async createOrder(createOrderInput: CreateOrderInput): Promise<Order> {
+    const { propertyId } = createOrderInput;
+
+    const property = await this.propertyService.getProperty(propertyId);
+
+    if (!property) {
+      throw new NotFoundException(
+        showErrorMessage(`Property with ID: ${propertyId} not found`),
+      );
+    }
+
+    const { customerId, landlordId } = createOrderInput.orderDetails;
+
+    const customer = await this.authService.getUserData(customerId);
+    const landlord = await this.authService.getUserData(landlordId);
+
+    if (!customer || !landlord) {
+      throw new NotFoundException(
+        showErrorMessage(`Customer or Landlord not found`),
+      );
+    }
+
     const newOrder = {
       ...createOrderInput,
       id: uuid(),
