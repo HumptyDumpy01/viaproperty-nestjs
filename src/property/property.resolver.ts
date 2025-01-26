722f4661af00253173159cf168f6bcde41fea909
HumptyDumpy01 <tuznikolas@gmail.com>
2025-01-26 15:29:47 +0200

    Applied protection to all property resolvers.

--- src/property/property.resolver.ts
+++ src/property/property.resolver.ts
@@ -16,6 +16,8 @@ import { PropertyCommentsService } from '../property-comments/property-comments.
 import { PropertyCommentsType } from '../property-comments/property-comments.type';
 import { PropertyQuestionsService } from '../property-questions/property-questions.service';
 import { PropertyQuestionsType } from '../property-questions/property-questions.type';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 
 // Specify the type of the resolver to which it would be attached.
 @Resolver((of) => PropertyType)
@@ -28,17 +30,20 @@ export class PropertyResolver {
   ) {}
 
   @Query(() => [PropertyType])
+  @UseGuards(AuthGuard)
   properties(@Args('filter', { nullable: true }) filter?: PropertyFilterInput) {
     return this.propertyService.getProperties(filter);
   }
 
   @Query((_returns) => PropertyType)
+  @UseGuards(AuthGuard)
   property(@Args('id') id: string) {
     return this.propertyService.getProperty(id);
   }
 
   // example of usage (mutation)
   @Mutation((_returns) => PropertyType)
+  @UseGuards(AuthGuard)
   addPropertyAdvert(@Args(`propertyInput`) propertyInput: PropertyInput) {
     // USE A SERVICE HERE. INJECT IT AS A DEP ONTO THIS CLASS
     return this.propertyService.createPropertyAdvert(propertyInput);
@@ -50,11 +55,13 @@ export class PropertyResolver {
   }
 
   @ResolveField(() => [PropertyCommentsType])
+  @UseGuards(AuthGuard)
   async reviews(@Parent() property: PropertyType) {
     return this.propertyCommentsService.propertyComments(property.id);
   }
 
   @ResolveField(() => [PropertyQuestionsType])
+  @UseGuards(AuthGuard)
   async questions(@Parent() property: PropertyType) {
     return this.propertyQuestionsService.getPropertyQuestionsByPropId(
       property.id,
