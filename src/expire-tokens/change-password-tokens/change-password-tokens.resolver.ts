8ec9861b3ebde9845e910f6d6e2ee585717ec110
HumptyDumpy01 <tuznikolas@gmail.com>
2025-02-03 13:37:11 +0200

    Implemented all the functionality to securely change user password.

--- src/expire-tokens/change-password-tokens/change-password-tokens.resolver.ts
+++ src/expire-tokens/change-password-tokens/change-password-tokens.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { ChangePasswordTokensService } from './change-password-tokens.service';
 import { CreateChangePasswordTokenInput } from './dto/create-change-password-token.input';
 import { ChangePasswordToken } from './object-types/change-password-token.object.type';
@@ -21,7 +21,7 @@ export class ChangePasswordTokensResolver {
     );
   }
 
-  @Query(() => ValidateTokenObjectType)
+  @Mutation(() => ValidateTokenObjectType)
   validateToken(
     @Args(`validateTokenInput`) validateTokenInput: ValidateTokenInput,
   ) {
